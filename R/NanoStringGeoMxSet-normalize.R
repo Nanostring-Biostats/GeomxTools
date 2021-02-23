6e503a63324376d3f1573b87b36bc75f7fc262d0
rvitancol <rvitancol@nanostring.com>
2021-02-23 07:45:30 +0000

    add RNA norm_methods

--- R/NanoStringGeoMxSet-normalize.R
+++ R/NanoStringGeoMxSet-normalize.R
@@ -1,10 +1,22 @@
-setMethod("normalize", "NanoStringGeomxSet",
-    function(object, norm_method=c("quantile", "neg", "hk"), data_type="RNA", 
+#' normalize
+#' @description normalize GeoMxSet using different normalization methods
+#' @param object name of the object class to perform normalization on
+#' @param norm_method the normalization method to be applied on the object
+#' @param data_type the data type of the object. Values maybe RNA, protein. 
+#' @param fromElt name of the assayDataElement to normalize
+#' @param toElt name of the assayDataElement to store normalized values
+#' @return a NanoStringGeoMxSet object with normalized counts and normalized factors
+#' @export
+#' @examples
+#' norm_object <- normalize(demoData)
+ 
+setMethod("normalize", "NanoStringGeoMxSet",
+    function(object, norm_method=c("quant", "neg", "hk"), data_type=c("RNA", "protein"), 
              fromElt = "exprs", toElt = "exprs_norm", ...) 
         {
         norm_method <- match.arg(norm_method)
-            switch(norm_method, "quantile" = {quantileNorm(object, data_type=data_type, quantile)}, 
-                "neg" = {neg_norm(object, data_type=data_type)}, 
+            switch(norm_method, "quant" = {quantileNorm(object, data_type=data_type)}, 
+                "neg" = {negNorm(object, data_type=data_type)}, 
                 "hk" = {hkNorm(object, data_type=data_type)})
         })
 
@@ -12,17 +24,29 @@ quantileNorm <- function(object, data_type, fromElt = "exprs" , toElt = "q_norm"
 { 
    #object<-checkQCFlags(object)
    ## Get quantile of counts for each sample
-   quantiles <- apply(exprs(object), 2, function(x) quantile(x,.75))
-   pData(object)[["qnormFactors"]] <- quantiles/ngeoMean(quantiles) 
-   assayDataElement(object, toElt) <- sweep(assayDataElement(object, fromElt), 2L, quantiles/ngeoMean(quantiles), FUN = "*")
+   qs <- apply(exprs(object), 2, function(x) quantile(x,.75))
+   pData(object)[["qnormFactors"]] <- qs/ngeoMean(qs) 
+   assayDataElement(object, toElt, validate=TRUE) <- sweep(assayDataElement(object, fromElt), 2L, qs/ngeoMean(qs), FUN = "*")
    return(object)
 } 
  
- negNorm <- function(object, data_type, fromElt = "exprs" , toElt = "exprs_norm") 
+ negNorm <- function(object, data_type, fromElt = "exprs" , toElt = "neg_norm") 
 { 
+     if (!featureType(object) == "Target")
+     {
+         stop ("Negative Background normalization is for collapsed data set.  
+            Run function aggregateCounts() to collapse the probes to targets.\n")
+     } else
+     {  
+         negsubset <- subset(object, subset = Codeclass %in% c("Negative01", "Negative"))
+         negs <- apply(exprs(negsubset), 2, function(x) ngeoMean(x))
+         pData(object)[["negnormFactors"]] <- negs/ngeoMean(negs)
+         assayDataElement(object, toElt) <- sweep(assayDataElement(object, fromElt), 2L, negs/ngeoMean(negs), FUN = "*")
+         return(object)
+     }
 } 
 
-hkNorm <- function(object, data_type) 
+hkNorm <- function(object, data_type, fromElt = "exprs" , toElt = "hk_norm") 
 { 
     housekeepers <- c('C1orf43','GPI','OAZ1','POLR2A','PSMB2','RAB7A',
                     'SDHA','SNRPD3','TBC1D10B','TPM4','TUBB','UBB')
@@ -40,11 +64,28 @@ hkNorm <- function(object, data_type)
     }
 } 
 
+#' Check QC Flags in the GeoMxSet and removes the probe or sample from the object
+#' @rdname checkQCFlags
+#' @param object name of the NanoStringGeoMxSet object to check the QC Flags
+#' @param ...  for other arguments 
+#' @return a NanoStringGeoMxSet object probes and samples failing QC removed
+#' @export
+#' @examples 
+#' DemoData <- checkQCFlags(Demodata)
 setGeneric("checkQCFlags", signature = c("object"), 
            function(object, ...)
                standardGeneric("checkQCFlags"))
 
-setMethod("checkQCFlags", "NanoStringGeomxSet",
+
+#' checkQCFlags
+#' @param NanoStringGeoMxSet 
+#' @param removeLowLocalOutliers logical, if TRUE it sets outlier counts to zero,  default is FALSE,
+#' @return NanoStringGeoMxSet
+#' @export
+#'
+#' @examples
+#' QCobject <- checkQCFlags(demoData)
+setMethod("checkQCFlags", "NanoStringGeoMxSet",
           function(object, removeLowLocalOutliers = FALSE,  ...)
           {
               ## Remove all samples that failed AOI QC should it have been run
@@ -79,5 +120,3 @@ setMethod("checkQCFlags", "NanoStringGeomxSet",
               }
             return(object)
           })
-
-
