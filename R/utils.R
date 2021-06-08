d6e7a38882a05a0f59465a5564cde18a183d6782
NicoleEO <Nicole.Ortogero@gmail.com>
2021-06-08 14:08:18 -0700

    Add function

--- R/utils.R
+++ R/utils.R
@@ -15,6 +15,21 @@ log10t <- function(x, thresh = 0.5) {
     log2(x)
 }
 
+#' Add one to all counts in an expression matrix
+#' 
+#' @param object name of the NanoStringGeoMxSet object to perform QC on
+#' @param elt expression matrix element in /code{assayDataElement}
+#'        to shift all counts by
+#' 
+#' @examples
+#' shiftOne(demoData)
+#' 
+#' @export
+#' 
+shiftOne <- function(object) {
+    assay
+}
+
 #NEO rewrite in data.tables for speed
 collapseCounts <- function(object) {
     probeCounts <- cbind(fData(object)[, c("TargetName", "Module")], 
