92494bf86e3460e4c5028c8b2c000c5fa1da169f
NicoleEO <Nicole.Ortogero@gmail.com>
2021-06-25 14:14:00 -0700

    Fix issue #74 and most #52, finish PR #68

--- R/readNanoStringGeoMxSet.R
+++ R/readNanoStringGeoMxSet.R
@@ -29,7 +29,7 @@ function(dccFiles,
   } else {
     pheno <- readxl::read_xlsx(phenoDataFile, col_names = TRUE, sheet = phenoDataSheet)
     pheno <- data.frame(pheno, stringsAsFactors = FALSE, check.names = FALSE)
-    j <- grep(phenoDataDccColName, colnames(pheno), ignore.case = TRUE)
+    j <- colnames(pheno)[colnames(pheno) == phenoDataDccColName]
     if (length(j) == 0L){
       stop("Column `phenoDataDccColName` not found in `phenoDataFile`")
     } else if (length(j) > 1L){
@@ -153,20 +153,30 @@ function(dccFiles,
   feature <- AnnotatedDataFrame(feature,
                                 dimLabels = c("featureNames", "featureColumns"))
 
-  # Create experimentData
-  if (!(is.null(experimentDataColNames))) {
-  experimentList<- lapply(experimentDataColNames,
-                            function(experimentDataColName)
-                              unique(S4Vectors::na.omit(pheno@data[[experimentDataColName]])))}
-  names(experimentList) <- experimentDataColNames
-  experiment <- Biobase::MIAME(name = "",
-                      other = c(experimentList, pkcHeader, list(shiftedByOne=FALSE)))
+    # Create experimentData
+    if (!(is.null(experimentDataColNames))) {
+        experimentList <- 
+            lapply(experimentDataColNames,
+                   function(experimentDataColName) {
+                       unique(S4Vectors::na.omit(pheno@data[[experimentDataColName]]))})
+        names(experimentList) <- experimentDataColNames
+        experiment <- 
+            Biobase::MIAME(name = "", 
+                           other = c(experimentList, 
+                                     pkcHeader, 
+                                     list(shiftedByOne=FALSE)))
+    } else {
+        experiment <- 
+            Biobase::MIAME(name = "",
+                           other = c(pkcHeader, 
+                                     list(shiftedByOne=FALSE)))
+    }
 
-  # Create annotation
-  annotation <- sort(sapply(strsplit(pkcFiles, "/"), function(x) x[length(x)]))
-  if(!identical(annotation, paste0(sort(unique(probeAssay[["Module"]])), ".pkc"))) {
-    stop("Name mismatch between pool and PKC files")
-  }
+    # Create annotation
+    annotation <- sort(sapply(strsplit(pkcFiles, "/"), function(x) x[length(x)]))
+    if(!identical(annotation, paste0(sort(unique(probeAssay[["Module"]])), ".pkc"))) {
+        stop("Name mismatch between pool and PKC files")
+    }
 
   # Create protocolData
   protocol <-
