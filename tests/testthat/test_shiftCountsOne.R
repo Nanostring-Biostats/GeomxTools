764f548e278b2234042882ee1d1f49976322ef1d
yuqiren <43080277+yuqiren@users.noreply.github.com>
2021-06-29 21:25:59 -0700

    Add files via upload

---
+++ tests/testthat/test_shiftCountsOne.R
@@ -0,0 +1,55 @@
+# devtools::install_github("https://github.com/Nanostring-Biostats/GeomxTools/", ref="dev", force = TRUE, dependencies = FALSE)
+
+library(NanoStringNCTools)
+library(GeomxTools)
+library(testthat)
+library(EnvStats)
+
+
+datadir <- system.file("extdata", "DSP_NGS_Example_Data",
+                       package="GeomxTools")
+DCCFiles <- dir(datadir, pattern=".dcc$", full.names=TRUE)
+PKCFiles <- unzip(zipfile = file.path(datadir,  "/pkcs.zip"))
+SampleAnnotationFile <- file.path(datadir, "annotations.xlsx")
+
+testData <-
+  suppressWarnings(readNanoStringGeoMxSet(dccFiles = DCCFiles, # QuickBase: readNanoStringGeomxSet, need to change it
+                                          pkcFiles = PKCFiles,
+                                          phenoDataFile = SampleAnnotationFile,
+                                          phenoDataSheet = "CW005",
+                                          phenoDataDccColName = "Sample_ID",
+                                          protocolDataColNames = c("aoi",
+                                                                   "cell_line",
+                                                                   "roi_rep",
+                                                                   "pool_rep",
+                                                                   "slide_rep"),
+                                          experimentDataColNames = c("panel")))
+
+protocolDataColNames <- c("aoi",
+                          "cell_line",
+                          "roi_rep",
+                          "pool_rep",
+                          "slide_rep")
+
+DCCFiles <- DCCFiles[!basename(DCCFiles) %in% unique(sData(testData)$NTC_ID)]
+testData2 <- shiftCountsOne(testData, elt="exprs", useDALogic=TRUE) 
+testData3 <- shiftCountsOne(testData, elt = "exprs", useDALogic = FALSE)
+
+
+
+# req 1: test that the counts from shiftCountsOne(..., useDALogic = TRUE) are correct:------
+
+fakeData = testData
+testthat::test_that("test that the counts from shiftCountsOne(..., useDALogic = TRUE) are correct", {
+  counts_0_add_1 = apply(exprs(fakeData), 1:2, function(x) ifelse(x==0, x+1, x))
+  expect_true(identical(counts_0_add_1, exprs(testData2)))
+})
+
+
+
+# req 2: test that the counts from shiftCountsOne(..., useDALogic = FALSE) are correct:------
+testthat::test_that("test that the counts from shiftCountsOne(..., useDALogic = FALSE) are correct", {
+  counts_all_add_1 = apply(exprs(testData), 1:2, function(x) x+1)
+  expect_true(identical(counts_all_add_1, exprs(testData3)))
+})
+
