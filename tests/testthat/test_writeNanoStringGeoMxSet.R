db4a012144a779be3ff71fd5dc54af1e02f6b947
Maddy Griswold <40255151+maddygriz@users.noreply.github.com>
2021-09-09 12:30:31 -0700

    add new test case for written DCC files

--- tests/testthat/test_writeNanoStringGeoMxSet.R
+++ tests/testthat/test_writeNanoStringGeoMxSet.R
@@ -21,19 +21,120 @@ demoData <-
                                                                    "slide_rep"),
                                           experimentDataColNames = c("panel")))
 
+<<<<<<< Updated upstream
+=======
+
+
+writeDir <-  "writeTest/"
+if(dir.exists(writeDir)){
+  unlink(writeDir, recursive = TRUE, force = TRUE)
+}
+
+NTCs <- unique(demoData@protocolData@data$NTC_ID)
+writtenDCCs <- basename(DCCFiles)
+writtenDCCs <- writtenDCCs[!writtenDCCs %in% NTCs]
+
+writeNanoStringGeoMxSet(demoData, dir = writeDir)
+
+# req 1: test DCC files are written after read in
+testthat::test_that("test DCC files are written after read in", {
+  expect_true(dir.exists(writeDir))
+  expect_true(all(dir(writeDir) %in% writtenDCCs))
+  expect_true(all(writtenDCCs %in% dir(writeDir)))
+})
+
+testData <-
+  suppressWarnings(readNanoStringGeoMxSet(dccFiles = dir(writeDir, full.names = TRUE), 
+                                          pkcFiles = PKCFiles,
+                                          phenoDataFile = SampleAnnotationFile,
+                                          phenoDataSheet = "CW005",
+                                          phenoDataDccColName = "Sample_ID",
+                                          protocolDataColNames = c("aoi",
+                                                                   "cell_line",
+                                                                   "roi_rep",
+                                                                   "pool_rep",
+                                                                   "slide_rep"),
+                                          experimentDataColNames = c("panel")))
+
+# req 2: test GeomxSet object from written DCC is identical to original
+testthat::test_that("test GeomxSet object from written DCC is identical to original", {
+  expect_identical(pData(testData), pData(demoData))
+  expect_identical(fData(testData), fData(demoData))
+  expect_identical(sData(testData)[,-which(colnames(sData(testData)) == "NTC")], 
+                   sData(demoData)[,-which(colnames(sData(demoData)) == "NTC")])
+  expect_identical(exprs(testData), exprs(demoData))
+  expect_identical(testData@experimentData, demoData@experimentData)
+})
+
+rm(testData)
+unlink(writeDir, recursive = TRUE, force = TRUE)
+
+>>>>>>> Stashed changes
 #Shift counts to one to mimic how DSPDA handles zero counts
 demoData <- shiftCountsOne(demoData, elt="exprs", useDALogic=TRUE) 
 
+<<<<<<< Updated upstream
 
 #QC flags
 demoData <- setSegmentQCFlags(demoData)
 demoData <- setBioProbeQCFlags(demoData)
+=======
+# req 3: test DCC files are written after shifting counts by one
+testthat::test_that("test DCC files are written after shifting counts by one", {
+  expect_true(dir.exists(writeDir))
+  expect_true(all(dir(writeDir) %in% writtenDCCs))
+  expect_true(all(writtenDCCs %in% dir(writeDir)))
+})
+unlink(writeDir, recursive = TRUE, force = TRUE)
+
+#QC flags
+demoData <- setSegmentQCFlags(demoData)
+writeNanoStringGeoMxSet(demoData, dir = writeDir)
+
+# req 4: test DCC files are written after SegmentQC
+testthat::test_that("test DCC files are written after SegmentQC", {
+  expect_true(dir.exists(writeDir))
+  expect_true(all(dir(writeDir) %in% writtenDCCs))
+  expect_true(all(writtenDCCs %in% dir(writeDir)))
+})
+unlink(writeDir, recursive = TRUE, force = TRUE)
+
+demoData <- setBioProbeQCFlags(demoData)
+writeNanoStringGeoMxSet(demoData, dir = writeDir)
+
+# req 5: test DCC files are written after Probe QC
+testthat::test_that("test DCC files are written after Probe QC", {
+  expect_true(dir.exists(writeDir))
+  expect_true(all(dir(writeDir) %in% writtenDCCs))
+  expect_true(all(writtenDCCs %in% dir(writeDir)))
+})
+unlink(writeDir, recursive = TRUE, force = TRUE)
+>>>>>>> Stashed changes
 
 #aggregate counts
 demoData <- aggregateCounts(demoData)
 
+<<<<<<< Updated upstream
+=======
+
+# req 6: test error occurs when writing set after aggregating counts
+testthat::test_that("test error occurs when writing set after aggregating counts", {
+  expect_error(writeNanoStringGeoMxSet(demoData, dir = writeDir))
+})
+unlink(writeDir, recursive = TRUE, force = TRUE)
+
+>>>>>>> Stashed changes
 #normalize
 demoData <- normalize(demoData , data_type="RNA", norm_method="quant",
                       desiredQuantile = .9, toElt = "q_norm")
 
+<<<<<<< Updated upstream
 writeNanoStringGeoMxSet(demoData, dir = "./")
+=======
+# req 7: test error occurs when writing set after aggregating counts
+testthat::test_that("test error occurs when writing set after manipulating target level counts", {
+  expect_error(writeNanoStringGeoMxSet(demoData, dir = writeDir))
+})
+unlink(writeDir, recursive = TRUE, force = TRUE)
+
+>>>>>>> Stashed changes
