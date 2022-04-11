c5e748a1ed3318f4923da57b48934c2dcb7a539e
Nicole Ortogero <20404635+NicoleEO@users.noreply.github.com>
2022-04-11 16:22:00 -0700

    Update NanoStringGeoMxSet-class.R
    update class version as well

--- R/NanoStringGeoMxSet-class.R
+++ R/NanoStringGeoMxSet-class.R
@@ -11,7 +11,7 @@ setClass("NanoStringGeoMxSet",
          prototype = prototype(
              new("VersionedBiobase",
                  versions = c(classVersion("ExpressionSet"),
-                              NanoStringGeoMxSet = "2.1.2")),
+                              NanoStringGeoMxSet = "2.1.3")),
              signatures = SignatureSet(),
              design = NULL,
              featureType = "Probe",
