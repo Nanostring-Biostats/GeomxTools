eebd89c68018863d0e6243351e1024352653a606
Maddy Griswold <mgriswold@nanostring.com>
2025-04-28 20:59:57 +0000

    cleaup up function

--- R/NanoStringGeoMxSet-autoplot.R
+++ R/NanoStringGeoMxSet-autoplot.R
@@ -100,31 +100,25 @@ qcProteinSignal <- function(object, neg.names=NULL) {
         neg.names <- iggNames(object)
     }
     
-    snr <- GeomxTools:::snrOrder(object, neg.names)
+    snr <- snrOrder(object, neg.names)
     
     protnames <- rownames(snr)
     
     nonZero <- snr
     nonZero[nonZero == 0] <- NA
     ylim <- log2(range(nonZero, na.rm = TRUE))
-    if(ylim[1L] == -Inf){
-      ylim[1L] <- 0
-    }
-    if(ylim[2L] == -Inf){
-      ylim[2L] <- 0
-    }
     rm(nonZero)
     
     fig <- function(){
         par(mar = c(11, 4, 2, 1))
-        boxplot(t(log2(snr)),
+        suppressWarnings(boxplot(t(log2(snr)),
                 las = 2,
                 outline = FALSE,
                 ylim = ylim,
                 names = protnames,
                 ylab = "Log2 signal-to-background ratio",
                 cex.axis = .85 - 0.3 * (nrow(snr) > 60)
-        )
+        ))
         axis(2, at = 1, labels = 1, las = 2, cex = 0.5)
         points(jitter(rep(1:nrow(snr), ncol(snr))),
                log2(snr),
